242cf3699b0e4371c910d58d00cd4f143262af6b
MatiasCripto <elpopllysca87@gmail.com>
2025-06-08 00:06:53 +0200

    permisos de app

--- android/app/src/main/java/com/construccionpro/app/MainActivity.java
+++ android/app/src/main/java/com/construccionpro/app/MainActivity.java
@@ -1,5 +1,23 @@
 package com.construccionpro.app;
 
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Configurar WebView para permisos de medios
+        getBridge().getWebView().setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                // Permitir automáticamente permisos de micrófono, cámara y ubicación
+                request.grant(request.getResources());
+            }
+        });
+    }
+}
